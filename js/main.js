096c2ef5f313c83d65ba03d56c5977c43916010b
Celia De Gregorio <celitadg@gmail.com>
2021-04-25 17:08:23 -0300

    Busquedas laborales

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(".trabajo-territorial").click(
+    function(){
+        $("#trabajo-territorial").show();
+        $("#logistica").hide();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").hide();
+
+    }
+);
+$(".logistica").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").show();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").hide();
+    }
+);
+$(".recaudador-fondos").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").hide();
+        $("#recaudador-fondos").show();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").hide();
+    }
+);
+$(".community-manager").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").hide();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").show();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").hide();
+    }
+);
+$(".editor-audiovisual").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").hide();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").show();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").hide();
+    }
+);
+$(".diseniador-grafico").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").hide();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").show();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").hide();
+    }
+);
+$(".asistente-twitter").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").hide();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").show();
+        $("#investigador-politico").hide();
+    }
+);
+$(".investigador-politico").click(
+    function(){
+        $("#trabajo-territorial").hide();
+        $("#logistica").hide();
+        $("#recaudador-fondos").hide();
+        $("#community-manager").hide();
+        $("#editor-audiovisual").hide();
+        $("#diseniador-grafico").hide();
+        $("#asistente-twitter").hide();
+        $("#investigador-politico").show();
+    }
+);
